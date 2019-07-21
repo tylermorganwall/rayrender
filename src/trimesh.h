e178987e8c0ff69feed0f152f5db9b7914fc860f
Tyler <tylermw@gmail.com>
2019-07-21 16:00:45 -0400

    Add different materials to obj support

--- src/trimesh.h
+++ src/trimesh.h
@@ -16,14 +16,21 @@ public:
     std::vector<tinyobj::shape_t > shapes;
     std::vector<tinyobj::material_t > materials;
     std::string warn, err;
+
     bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, inputfile.c_str());
+
     if(ret) {
       int n = 0;
       for (size_t s = 0; s < shapes.size(); s++) {
         n += shapes[s].mesh.num_face_vertices.size();
       }
       std::vector<unsigned char* > obj_materials(materials.size()+1);
+      std::vector<vec3 > diffuse_materials(materials.size()+1);
+      std::vector<vec3 > specular_materials(materials.size()+1);
+      std::vector<float > ior_materials(materials.size()+1);
       std::vector<bool > has_diffuse(materials.size()+1);
+      std::vector<bool > has_transparency(materials.size()+1);
+      std::vector<bool > has_single_diffuse(materials.size()+1);
       std::vector<vec3 > image_dim(materials.size()+1);
       std::vector<int > nx_mat(materials.size()+1);
       std::vector<int > ny_mat(materials.size()+1);
@@ -31,12 +38,22 @@ public:
       for (size_t i = 0; i < materials.size(); i++) {
         if(strlen(materials[i].diffuse_texname.c_str()) > 0) {
           obj_materials[i] = stbi_load(materials[i].diffuse_texname.c_str(), &nx, &ny, &nn, 0);
-          // Rcpp::Rcout << materials[i].diffuse_texname << ": " <<  nx << " " << nx << " " << "\n";
           has_diffuse[i] = true;
+          has_single_diffuse[i] = false;
           nx_mat[i] = nx;
           ny_mat[i] = ny;
+        } else if (sizeof(materials[i].diffuse) == 12) {
+          diffuse_materials[i] = vec3(materials[i].diffuse[0],materials[i].diffuse[1],materials[i].diffuse[2]);
+          has_diffuse[i] = true;
+          has_single_diffuse[i] = true;
         } else {
           has_diffuse[i] = false;
+          has_single_diffuse[i] = false;
+        }
+        if(materials[i].dissolve < 1) {
+          specular_materials[i] = vec3(materials[i].specular[0],materials[i].specular[1],materials[i].specular[2]);
+          ior_materials[i] = materials[i].ior;
+          has_transparency[i] = true; 
         }
       }
       bool has_normals = attrib.normals.size() > 0 ? true : false;
@@ -80,22 +97,36 @@ public:
           
           // per-face material
           // shapes[s].mesh.material_ids[f];
+          material *tex;
           if(has_normals) {
-            triangles[currenttri] = new triangle(tris[0],tris[1],tris[2],normals[0],normals[1],normals[2], 
-                                        new lambertian(new triangle_image_texture(obj_materials[material_num],
-                                                                                  nx_mat[material_num], ny_mat[material_num],
-                                                                                  tx[0],ty[0],
-                                                                                  tx[1],ty[1],
-                                                                                  tx[2],ty[2])));
+            if(has_transparency[material_num]) {
+              tex = new dielectric(specular_materials[material_num], ior_materials[material_num], rng);;
+            } else if(has_diffuse[material_num]) {
+              if(has_single_diffuse[material_num]) {
+                tex = new lambertian(new constant_texture(diffuse_materials[material_num]));
+              } else {
+                tex = new lambertian(new triangle_image_texture(obj_materials[material_num],
+                                      nx_mat[material_num], ny_mat[material_num],
+                                      tx[0],ty[0], tx[1],ty[1],tx[2],ty[2]));
+              }
+            } else {
+              tex = new lambertian(new constant_texture(vec3(1,1,1)));
+            }
+            triangles[currenttri] = new triangle(tris[0],tris[1],tris[2],normals[0], normals[1], normals[2], tex);
             currenttri++;
           } else {
             if(has_diffuse[material_num]) {
-              triangles[currenttri] = new triangle(tris[0],tris[1],tris[2],
+              if(has_single_diffuse[material_num]) {
+                triangles[currenttri] = new triangle(tris[0],tris[1],tris[2],
+                                                     new lambertian(new constant_texture(diffuse_materials[material_num])));
+              } else {
+                triangles[currenttri] = new triangle(tris[0],tris[1],tris[2],
                                           new lambertian(new triangle_image_texture(obj_materials[material_num],
                                                                                     nx_mat[material_num],ny_mat[material_num],
                                                                                     tx[0],ty[0],
                                                                                     tx[1],ty[1],
                                                                                     tx[2],ty[2])));
+              }
             } else {
               triangles[currenttri] = new triangle(tris[0],tris[1],tris[2],new lambertian(new constant_texture(vec3(1,1,1))));
             }
@@ -111,7 +142,7 @@ public:
     std::vector<tinyobj::shape_t > shapes;
     std::vector<tinyobj::material_t > materials;
     std::string warn, err;
-    
+
     bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, inputfile.c_str());
     //need to define else
     if(ret) {
@@ -130,9 +161,11 @@ public:
         for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
           for (size_t v = 0; v < 3; v++) {
             tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+            
             tris[v] = vec3(attrib.vertices[3*idx.vertex_index+0],
                            attrib.vertices[3*idx.vertex_index+1],
                            attrib.vertices[3*idx.vertex_index+2])*scale;
+
             if(has_normals) {
               normals[v] = vec3(attrib.normals[3*idx.normal_index+0],
                                 attrib.normals[3*idx.normal_index+1],
