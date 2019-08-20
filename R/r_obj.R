b31200354b93c50f61951cee77be9c8a9dca194a
Tyler <tylermw@gmail.com>
2019-08-20 08:49:04 -0400

    Added `r_obj()` function to return location of R.obj file

---
+++ R/r_obj.R
@@ -0,0 +1,19 @@
+#' R 3D Model
+#' 
+#' 3D obj model of the letter R, to be used with `obj_model()`
+#' 
+#' @return File location of the R.obj file (saved with a .txt extension)
+#' @export
+#'
+#' @examples
+#' #Load and render the included example R object file.
+#' \dontrun{
+#' generate_ground(material = lambertian(noise = TRUE, noisecolor = "grey20")) %>%
+#'   add_object(sphere(x = 2, y = 3, z = 2, radius = 1,
+#'                     material = lambertian(lightintensity = 10, implicit_sample = TRUE))) %>%
+#'   add_object(obj_model(r_obj(), y = -1, material = lambertian(color="red"))) %>%
+#'   render_scene(parallel=TRUE, lookfrom = c(0, 1, 10), clamp_value = 5, samples = 200)
+#' }
+r_obj = function() {
+  system.file("extdata", "r_obj.txt", package="rayrender")
+}
