ece697f792774c9438532e6a6cb2c3a386d0ea91
Tyler <tylermw@gmail.com>
2019-12-01 14:34:18 -0500

    add micro_pdf

--- src/pdf.h
+++ src/pdf.h
@@ -31,6 +31,37 @@ public:
   onb uvw;
 };
 
+class micro_pdf : public pdf {
+public:
+  micro_pdf(const vec3& w, Float roughness, vec3 wo_) {
+    wo_.make_unit_vector();
+    wo = wo_;
+    a2 = roughness * roughness * roughness * roughness;
+    uvw.build_from_w(w);
+  }
+  virtual Float value(const vec3& direction, random_gen& rng) {
+    Float cosTheta = dot(unit_vector(direction), uvw.w());;
+    Float expval = (a2 - 1.0f) * cosTheta + 1;
+    Float D = a2 / (M_PI * expval * expval);
+    // vec3 wi = wo - 2.0f * dot(uvw.w(), direction) * uvw.w();
+    return(D/4);
+  }
+  virtual vec3 generate(random_gen& rng) {
+    Float r0 = rng.unif_rand();
+    Float theta = std::acos(std::sqrt((1 - r0) / ((a2-1)*r0 + 1)));
+    Float phi = 2 * M_PI * rng.unif_rand();
+    vec3 wm = vec3(std::sin(theta) * std::cos(phi),
+                   std::cos(theta),
+                   std::sin(theta) * std::sin(phi));
+    wm.make_unit_vector();
+    vec3 wi = 2.0f * dot(wo, wm) * wm - wo;
+    return(uvw.local(wi));
+  }
+  onb uvw;
+  Float a2;
+  vec3 wo;
+};
+
 class hitable_pdf : public pdf {
 public:
   hitable_pdf(hitable *p, const vec3& origin) : ptr(p), o(origin) {}
