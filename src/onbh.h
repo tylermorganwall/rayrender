870773898c6ac189002d6b05db6e5eea61db708d
Tyler <tylermw@gmail.com>
2020-05-20 21:07:36 -0400

    Simplify ONB code

--- src/onbh.h
+++ src/onbh.h
@@ -22,12 +22,7 @@ public:
 
 void onb::build_from_w(const vec3& n) {
   axis[2] = unit_vector(n);
-  vec3 a;
-  if(fabs(w().x()) > 0.9) {
-    a = vec3(0,1,0);
-  } else {
-    a = vec3(1,0,0);
-  }
+  vec3 a = (fabs(w().x()) > 0.9) ? vec3(0,1,0) : vec3(1,0,0);
   axis[1] = unit_vector(cross(w(),a));
   axis[0] = cross(w(), v());
 }
