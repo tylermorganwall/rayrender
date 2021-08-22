d965132c92ab068843bd3b73030a4da3593b6ae8
Tyler <tylermw@gmail.com>
2021-08-22 18:12:33 -0400

    -Working ellipsoid transformations

--- src/ellipsoid.cpp
+++ src/ellipsoid.cpp
@@ -2,7 +2,8 @@
 
 
 bool ellipsoid::hit(const ray& r, Float t_min, Float t_max, hit_record& rec, random_gen& rng) {
-  ray scaled_ray(r.origin() * point3f(inv_axes) + -center, r.direction() * inv_axes);
+  ray r2 = (*WorldToObject)(r);
+  ray scaled_ray(r2.origin() * point3f(inv_axes) + -center, r2.direction() * inv_axes);
   Float a = dot(scaled_ray.direction(), scaled_ray.direction());
   Float b = 2 * dot(scaled_ray.origin(), scaled_ray.direction()); 
   Float c = dot(scaled_ray.origin(),scaled_ray.origin()) - 1;
@@ -64,11 +65,14 @@ bool ellipsoid::hit(const ray& r, Float t_min, Float t_max, hit_record& rec, ran
       rec.bump_normal = rec.normal + normal3f(bvbu.x() * rec.dpdu + bvbu.y() * rec.dpdv); 
       rec.bump_normal *= inv_axes;
       rec.bump_normal.make_unit_vector();
+      rec.bump_normal = (*ObjectToWorld)(rec.bump_normal);
     }
     rec.normal *= inv_axes;
     rec.normal.make_unit_vector();
     rec.p *= 1/rec.p.length() * axes;
     
+    rec.p = (*ObjectToWorld)(rec.p);
+    rec.normal = (*ObjectToWorld)(rec.normal);
     return(true);
   }
   if(temp2 < t_max && temp2 > t_min && second_is_hit) {
@@ -95,6 +99,7 @@ bool ellipsoid::hit(const ray& r, Float t_min, Float t_max, hit_record& rec, ran
       rec.bump_normal = rec.normal + normal3f(bvbu.x() * rec.dpdu + bvbu.y() * rec.dpdv); 
       rec.bump_normal *= inv_axes;
       rec.bump_normal.make_unit_vector();
+      rec.bump_normal = (*ObjectToWorld)(rec.bump_normal);
     }
     rec.normal *= inv_axes;
     rec.normal.make_unit_vector();
@@ -104,6 +109,8 @@ bool ellipsoid::hit(const ray& r, Float t_min, Float t_max, hit_record& rec, ran
       rec.normal = -rec.normal;
       rec.bump_normal = -rec.bump_normal;
     }
+    rec.p = (*ObjectToWorld)(rec.p);
+    rec.normal = (*ObjectToWorld)(rec.normal);
     return(true);
   }
   return(false);
@@ -173,11 +180,14 @@ bool ellipsoid::hit(const ray& r, Float t_min, Float t_max, hit_record& rec, Sam
       rec.bump_normal = rec.normal + normal3f(bvbu.x() * rec.dpdu + bvbu.y() * rec.dpdv); 
       rec.bump_normal *= inv_axes;
       rec.bump_normal.make_unit_vector();
+      rec.bump_normal = (*ObjectToWorld)(rec.bump_normal);
     }
     rec.normal *= inv_axes;
     rec.normal.make_unit_vector();
     rec.p *= 1/rec.p.length() * axes;
     
+    rec.p = (*ObjectToWorld)(rec.p);
+    rec.normal = (*ObjectToWorld)(rec.normal);
     return(true);
   }
   if(temp2 < t_max && temp2 > t_min && second_is_hit) {
@@ -204,6 +214,7 @@ bool ellipsoid::hit(const ray& r, Float t_min, Float t_max, hit_record& rec, Sam
       rec.bump_normal = rec.normal + normal3f(bvbu.x() * rec.dpdu + bvbu.y() * rec.dpdv); 
       rec.bump_normal *= inv_axes;
       rec.bump_normal.make_unit_vector();
+      rec.bump_normal = (*ObjectToWorld)(rec.bump_normal);
     }
     rec.normal *= inv_axes;
     rec.normal.make_unit_vector();
@@ -213,16 +224,19 @@ bool ellipsoid::hit(const ray& r, Float t_min, Float t_max, hit_record& rec, Sam
       rec.normal = -rec.normal;
       rec.bump_normal = -rec.bump_normal;
     }
+    rec.p = (*ObjectToWorld)(rec.p);
+    rec.normal = (*ObjectToWorld)(rec.normal);
     return(true);
   }
   return(false);
 }
 
-
+//Not great
 Float ellipsoid::pdf_value(const point3f& o, const vec3f& v, random_gen& rng, Float time) {
   hit_record rec;
   if(this->hit(ray(o,v), 0.001, FLT_MAX, rec, rng)) {
-    Float cos_theta_max = sqrt(1 - 1/(center - o).squared_length());
+    point3f o2 = (*WorldToObject)(o);
+    Float cos_theta_max = sqrt(1 - 1/(center - o2).squared_length());
     Float solid_angle = 2 * M_PI * (1-cos_theta_max) * largest_proj_axis ;
     return(1/solid_angle);
   } else {
@@ -231,10 +245,12 @@ Float ellipsoid::pdf_value(const point3f& o, const vec3f& v, random_gen& rng, Fl
 }
 
 
+//Not great
 Float ellipsoid::pdf_value(const point3f& o, const vec3f& v, Sampler* sampler, Float time) {
   hit_record rec;
   if(this->hit(ray(o,v), 0.001, FLT_MAX, rec, sampler)) {
-    Float cos_theta_max = sqrt(1 - 1/(center - o).squared_length());
+    point3f o2 = (*WorldToObject)(o);
+    Float cos_theta_max = sqrt(1 - 1/(center - o2).squared_length());
     Float solid_angle = 2 * M_PI * (1-cos_theta_max) * largest_proj_axis ;
     return(1/solid_angle);
   } else {
@@ -243,22 +259,83 @@ Float ellipsoid::pdf_value(const point3f& o, const vec3f& v, Sampler* sampler, F
 }
 
 vec3f ellipsoid::random(const point3f& o, random_gen& rng, Float time) {
-  vec3f direction = center - o;
-  Float distance_squared = direction.squared_length();
+  point3f pCenter = (*ObjectToWorld)(point3f(0, 0, 0));
+  vec3f wc = pCenter - o;
+  Float dc = wc.length();
+  Float invDc = 1 / dc;
+  wc *= dc;
+  
   onb uvw;
-  uvw.build_from_w(direction);
-  return(uvw.local_to_world(rng.random_to_sphere(radius,distance_squared) * inv_axes));
+  uvw.build_from_w(wc);
+  vec2f u = vec2f(rng.unif_rand(),rng.unif_rand());
+  
+  Float sinThetaMax = radius * invDc;
+  Float sinThetaMax2 = sinThetaMax * sinThetaMax;
+  Float invSinThetaMax = 1 / sinThetaMax;
+  
+  Float cosThetaMax = std::sqrt(std::max((Float)0.f, 1 - sinThetaMax2));
+  
+  Float cosTheta  = (cosThetaMax - 1) * u[0] + 1;
+  Float sinTheta2 = 1 - cosTheta * cosTheta;
+  
+  if (sinThetaMax2 < 0.00068523f /* sin^2(1.5 deg) */) {
+    /* Fall back to a Taylor series expansion for small angles, where
+     the standard approach suffers from severe cancellation errors */
+    sinTheta2 = sinThetaMax2 * u[0];
+    cosTheta = std::sqrt(1 - sinTheta2);
+  }
+  
+  // Compute angle $\alpha$ from center of sphere to sampled point on surface
+  Float cosAlpha = sinTheta2 * invSinThetaMax +
+    cosTheta * std::sqrt(std::fmax((Float)0.f, 1.f - sinTheta2 * invSinThetaMax * invSinThetaMax));
+  Float sinAlpha = std::sqrt(std::fmax((Float)0.f, 1.f - cosAlpha*cosAlpha));
+  Float phi = u.e[1] * 2 * M_PI;
+  
+  // Compute surface normal and sampled point on sphere
+  vec3f nWorld = SphericalDirection(sinAlpha, cosAlpha, phi, -uvw.u(), -uvw.v(), -uvw.w()) * inv_axes;
+  point3f pWorld = pCenter + radius * point3f(nWorld.x(), nWorld.y(), nWorld.z());
+  return (pWorld-o);
 }
 
 vec3f ellipsoid::random(const point3f& o, Sampler* sampler, Float time) {
-  vec3f direction = center - o;
-  Float distance_squared = direction.squared_length();
+  point3f pCenter = (*ObjectToWorld)(point3f(0, 0, 0));
+  vec3f wc = pCenter - o;
+  Float dc = wc.length();
+  Float invDc = 1 / dc;
+  wc *= dc;
+  
   onb uvw;
-  uvw.build_from_w(direction);
-  return(uvw.local_to_world(rand_to_sphere(radius,distance_squared, sampler->Get2D()) * inv_axes));
+  uvw.build_from_w(wc);
+  vec2f u = sampler->Get2D();
+  Float sinThetaMax = radius * invDc;
+  Float sinThetaMax2 = sinThetaMax * sinThetaMax;
+  Float invSinThetaMax = 1 / sinThetaMax;
+  
+  Float cosThetaMax = std::sqrt(std::max((Float)0.f, 1 - sinThetaMax2));
+  
+  Float cosTheta  = (cosThetaMax - 1) * u[0] + 1;
+  Float sinTheta2 = 1 - cosTheta * cosTheta;
+  
+  if (sinThetaMax2 < 0.00068523f /* sin^2(1.5 deg) */) {
+    /* Fall back to a Taylor series expansion for small angles, where
+     the standard approach suffers from severe cancellation errors */
+    sinTheta2 = sinThetaMax2 * u[0];
+    cosTheta = std::sqrt(1 - sinTheta2);
+  }
+  
+  // Compute angle $\alpha$ from center of sphere to sampled point on surface
+  Float cosAlpha = sinTheta2 * invSinThetaMax +
+    cosTheta * std::sqrt(std::fmax((Float)0.f, 1.f - sinTheta2 * invSinThetaMax * invSinThetaMax));
+  Float sinAlpha = std::sqrt(std::fmax((Float)0.f, 1.f - cosAlpha*cosAlpha));
+  Float phi = u.e[1] * 2 * M_PI;
+  
+  // Compute surface normal and sampled point on sphere
+  vec3f nWorld = SphericalDirection(sinAlpha, cosAlpha, phi, -uvw.u(), -uvw.v(), -uvw.w()) * inv_axes;
+  point3f pWorld = pCenter + radius * point3f(nWorld.x(), nWorld.y(), nWorld.z());
+  return (pWorld-o);
 }
 
 bool ellipsoid::bounding_box(Float t0, Float t1, aabb& box) const {
-  box = aabb(center + -point3f(radius,radius,radius) * axes, center + point3f(radius,radius,radius) * axes);
+  box = (*ObjectToWorld)(aabb(center + -point3f(radius,radius,radius) * axes, center + point3f(radius,radius,radius) * axes));
   return(true);
 }
