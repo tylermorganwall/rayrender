ecf6d463ee8f75771a206f2d9e240a88581cc062
Tyler <tylermw@gmail.com>
2021-10-26 11:39:43 -0400

    -Update color.cpp to break out when pdf_val == 0

--- src/color.cpp
+++ src/color.cpp
@@ -81,9 +81,12 @@ point3f color(const ray& r, hitable *world, hitable_list *hlist,
         r2 = ray(OffsetRayOrigin(offset_p, hrec.pError, hrec.normal, dir), dir, r2.pri_stack, r2.time());
         
         pdf_val = p.value(dir, rng, r2.time()); //generates a pdf value based the intersection point and the mixture pdf
-        // RcppThread::Rcout << i << " " << hrec.shape->GetName() << " " <<  throughput << " " << pdf_val << "\n";
+
+        if(pdf_val == 0) {
+          break;
+        }
         
-        if((dir.x() == 0 && dir.y() == 0 && dir.z() == 0) || pdf_val == 0) {
+        if((dir.x() == 0 && dir.y() == 0 && dir.z() == 0)) {
           break;
         }
         
