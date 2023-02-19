2da2f5b14da79816026d30151c1f1c98d9f3b09b
tylermorganwall <tylermorganwall@users.noreply.github.com>
2023-02-18 19:28:18 -0500

    -Remove snapshot tests and replace with checks for no errors

--- tests/testthat/test-basic-rendering.R
+++ tests/testthat/test-basic-rendering.R
@@ -1,13 +1,13 @@
-save_test_png = function(code, path) {
-  code
-  path
-}
-
-compare_image = function(path1, path2) {
-  image1 = png::readPNG(path1)
-  image2 = png::readPNG(path2)
-  return(identical(image1, image2))
-}
+# save_test_png = function(code, path) {
+#   code
+#   path
+# }
+# 
+# compare_image = function(path1, path2) {
+#   image1 = png::readPNG(path1)
+#   image2 = png::readPNG(path2)
+#   return(identical(image1, image2))
+# }
 
 run_tests = function(func, argument_grid, plot_prefix="", ...) {
   stopifnot(inherits(argument_grid,"data.frame"))
@@ -15,21 +15,26 @@ run_tests = function(func, argument_grid, plot_prefix="", ...) {
   for(i in seq_len(nrow(argument_grid))){
     set.seed(123)
     args = unlist(argument_grid[i,], recursive = FALSE)
-    test_filename = paste0(sprintf("%s-%s", substr(names(args),1,5), args),collapse="_")
-    test_filename = sprintf("%s_test%i.png",
-                            plot_prefix , i)
-    path = tempfile(fileext = ".png")
+    # test_filename = paste0(sprintf("%s-%s", substr(names(args),1,5), args),collapse="_")
+    # test_filename = sprintf("%s_test%i.png",
+    #                         plot_prefix , i)
+    # path = tempfile(fileext = ".png")
     
     args = append(args, ...)
-    args = append(args, list(filename = path))
-    save_test_png(do.call(func, args = args), path) |> 
-      suppressMessages() |> 
-      suppressWarnings() |> 
-      expect_snapshot_file(name = test_filename, compare = compare_image)
+    args = append(args, list(interactive = FALSE, 
+                             verbose = FALSE, 
+                             preview = FALSE, 
+                             progress = FALSE))
+    
+    # args = append(args, list(filename = path))
+    do.call(func, args = args) |> 
+      expect_no_error()
+      # expect_snapshot_file(name = test_filename, compare = compare_image)
+      
   }
 }
 
-test_that("plot_3d plots basic options", {
+test_that("render_scene basic options", {
   skip_on_cran()
   
   scene1 = generate_ground(depth=-0.5, material = diffuse(color="white", checkercolor="darkgreen")) |> 
@@ -47,8 +52,7 @@ test_that("plot_3d plots basic options", {
     add_object(pig(y=0.5,x=1,scale = 0.3,angle=c(0,-70,0))) |> 
     add_object(sphere(x=10,y=5,z=10,radius=2,material = light(intensity = 40))) |> 
     add_object(sphere(x=-0.4,y=2,z=-3,radius=0.02,material = light(intensity = 10000))) 
-  options("cores" = 1)
-  
+
   render_args_basic = expand.grid(width         = list(50,100),
                                   height        = list(50,100),
                                   clamp_value   = list(Inf, 10),
@@ -57,9 +61,7 @@ test_that("plot_3d plots basic options", {
   run_tests("render_scene", render_args_basic, plot_prefix = "basic", 
             list(scene = scene1,
                  samples = 16,
-                 ortho_dimensions = c(3,3),
-                 preview  = FALSE, 
-                 progress = FALSE))
+                 ortho_dimensions = c(3,3)))
   
   render_args_camera_info = expand.grid(lookfrom = list(c(0,1,10),c(-10,1,10)),
                                         lookat   = list(c(0,0,0), c(0,0.75,0)),
@@ -70,9 +72,7 @@ test_that("plot_3d plots basic options", {
   run_tests("render_scene", render_args_camera_info, plot_prefix = "cam_info", 
             list(scene = scene1,
                  samples = 16,
-                 ortho_dimensions = c(3,3),
-                 preview  = FALSE, 
-                 progress = FALSE))
+                 ortho_dimensions = c(3,3)))
   
   render_args_post_processing_info = expand.grid(bloom       = list(FALSE, 10),
                                                  clamp_value = list(Inf, 10),
@@ -82,9 +82,7 @@ test_that("plot_3d plots basic options", {
   run_tests("render_scene", render_args_post_processing_info, plot_prefix = "post", 
             list(scene = scene1,
                  samples = 16,
-                 ortho_dimensions = c(3,3),
-                 preview  = FALSE, 
-                 progress = FALSE))
+                 ortho_dimensions = c(3,3)))
   
   render_args_debug = expand.grid(debug_channel= list("none","depth","normals","uv",
                                                       "variance","dpdu","dpdv","color"),
@@ -93,9 +91,7 @@ test_that("plot_3d plots basic options", {
   run_tests("render_scene", render_args_debug, plot_prefix = "debug", 
             list(scene = scene1,
                  samples = 16,
-                 ortho_dimensions = c(3,3),
-                 preview  = FALSE, 
-                 progress = FALSE))
+                 ortho_dimensions = c(3,3)))
   
   render_sampling_args = expand.grid(samples = list(1,16),
                                      sample_method = list("sobol","sobol_blue","random", "stratified"),
@@ -106,9 +102,7 @@ test_that("plot_3d plots basic options", {
   run_tests("render_scene", render_sampling_args, plot_prefix = "sampling", 
             list(scene = scene1,
                  width = 200,
-                 height = 200,
-                 preview  = FALSE, 
-                 progress = FALSE))
+                 height = 200))
   
   render_termination_args = expand.grid(max_depth     = list(NA, 10, 100),
                                         roulette_active_depth = list(100,10,3))
@@ -117,9 +111,7 @@ test_that("plot_3d plots basic options", {
             list(scene = scene1,
                  samples = 16,
                  width = 200,
-                 height = 200,
-                 preview  = FALSE, 
-                 progress = FALSE))
+                 height = 200))
   
   render_realistic_camera_args = expand.grid(camera_description_file = list("telephoto", "50mm","wide","fisheye"),
                                              aperture = list(1,10),
@@ -133,9 +125,7 @@ test_that("plot_3d plots basic options", {
                  samples = 16,
                  iso = 10000,
                  width = 200,
-                 height = 200,
-                 preview  = FALSE, 
-                 progress = FALSE))
+                 height = 200))
                                      
   
   expect_error(render_scene(scene1,lookat=c(0,0.5,0), width  = 3, bloom=TRUE))
