36a671b1151ec3f9d1e1d04563d35621c9598788
tylermorganwall <tylermorganwall@users.noreply.github.com>
2024-10-02 23:26:12 +0100

    -Initialize SIMD components in ray for reuse in triangles

--- src/ray.h
+++ src/ray.h
@@ -6,6 +6,7 @@
 #include "point3.h"
 #include <vector>
 #include "mathinline.h"
+#include "simd.h"
 
 class dielectric;
 
@@ -46,6 +47,26 @@ class ray {
       sign[0] = (inv_dir.x() < 0);
       sign[1] = (inv_dir.y() < 0);
       sign[2] = (inv_dir.z() < 0);
+      kz = MaxDimension(Abs(B));
+      kx = kz + 1;
+      if (kx == 3) kx = 0;
+      ky = kx + 1;
+      if (ky == 3) ky = 0;
+      dPermuted = Permute(B, kx, ky, kz);
+      Sx = -dPermuted.x() / dPermuted.z();
+      Sy = -dPermuted.y() / dPermuted.z();
+      Sz = 1.f / dPermuted.z();
+#ifdef RAYSIMD
+      origin4[0] = simd_set1(a.x());
+      origin4[1] = simd_set1(a.y());
+      origin4[2] = simd_set1(a.z());
+      inv_dir_pad4[0] = simd_set1(inv_dir_pad.x());
+      inv_dir_pad4[1] = simd_set1(inv_dir_pad.y());
+      inv_dir_pad4[2] = simd_set1(inv_dir_pad.z());
+      maskPos4[0] = simd_cmpge(inv_dir_pad4[0], simd_set1(0.0f));
+      maskPos4[1] = simd_cmpge(inv_dir_pad4[1], simd_set1(0.0f));
+      maskPos4[2] = simd_cmpge(inv_dir_pad4[2], simd_set1(0.0f));
+#endif
     }
     ray(const point3f& a, const vec3f& b,  std::vector<dielectric* > *priority2, 
         Float ti = 0.0, Float tmax = Infinity) {
@@ -61,6 +82,26 @@ class ray {
       sign[1] = (inv_dir.y() < 0);
       sign[2] = (inv_dir.z() < 0);
       pri_stack = priority2;
+      kz = MaxDimension(Abs(B));
+      kx = kz + 1;
+      if (kx == 3) kx = 0;
+      ky = kx + 1;
+      if (ky == 3) ky = 0;
+      dPermuted = Permute(B, kx, ky, kz);
+      Sx = -dPermuted.x() / dPermuted.z();
+      Sy = -dPermuted.y() / dPermuted.z();
+      Sz = 1.f / dPermuted.z();
+#ifdef RAYSIMD
+      origin4[0] = simd_set1(a.x());
+      origin4[1] = simd_set1(a.y());
+      origin4[2] = simd_set1(a.z());
+      inv_dir_pad4[0] = simd_set1(inv_dir_pad.x());
+      inv_dir_pad4[1] = simd_set1(inv_dir_pad.y());
+      inv_dir_pad4[2] = simd_set1(inv_dir_pad.z());
+      maskPos4[0] = simd_cmpge(inv_dir_pad4[0], simd_set1(0.0f));
+      maskPos4[1] = simd_cmpge(inv_dir_pad4[1], simd_set1(0.0f));
+      maskPos4[2] = simd_cmpge(inv_dir_pad4[2], simd_set1(0.0f));
+#endif
     }
     point3f operator()(Float t) const { return A + B * t; }
     
@@ -76,8 +117,17 @@ class ray {
     vec3f B;
     vec3f inv_dir;
     vec3f inv_dir_pad;
+  #ifdef RAYSIMD
+    FVec4 origin4[3];
+    FVec4 inv_dir_pad4[3];
+    SimdMask maskPos4[3];
+  #endif
+
     int sign[3];
     Float _time;
+    Float Sx, Sy, Sz;
+    int kx, ky, kz;
+    vec3f dPermuted;
     mutable Float tMax;
     std::vector<dielectric*> *pri_stack;
 };
