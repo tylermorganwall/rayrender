30df2ab63d7e1b1d36cadf9b260d8beef4a12b68
tylermorganwall <tylermorganwall@users.noreply.github.com>
2024-12-29 12:06:33 -0500

    -Fix test output

--- tests/testthat/test-consistent-rendering.R
+++ tests/testthat/test-consistent-rendering.R
@@ -6,7 +6,7 @@ test_that("Test rendering with and without OIDN", {
     render_scene(samples=16, denoise=F, print_debug_info = TRUE) ->
   rgb_output
   if(abs(sum(rgb_output) - 232160.35) > 0.01) {
-    message(sprintf("Sum of rgb_output is %f"), sum(rgb_output))
+    message(sprintf("Sum of rgb_output is %f", sum(rgb_output)))
   }
   expect_true(abs(sum(rgb_output) - 232160.35) < 0.01)
   if(cppdef_HAS_OIDN()()) {
@@ -18,7 +18,7 @@ test_that("Test rendering with and without OIDN", {
       render_scene(samples=16, preview = FALSE) ->
     rgb_output_oidn
     if(abs(sum(rgb_output_oidn) - 235740.581) > 0.01) {
-      message(sprintf("Sum of rgb_output_oidn is %f"), sum(rgb_output_oidn))
+      message(sprintf("Sum of rgb_output_oidn is %f", sum(rgb_output_oidn)))
     }
     expect_true(abs(sum(rgb_output_oidn) - 235740.581) < 0.01)
   }
