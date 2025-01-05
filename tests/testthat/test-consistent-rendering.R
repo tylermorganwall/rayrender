bff2c9a4c0fec9ca29a9627ce5ff925962bde125
tylermorganwall <tylermorganwall@users.noreply.github.com>
2025-01-05 14:57:07 -0500

    -Add snapshot tests for integrators

--- tests/testthat/test-consistent-rendering.R
+++ tests/testthat/test-consistent-rendering.R
@@ -36,3 +36,28 @@ test_that("Test rendering with and without OIDN, snapshot", {
     expect_snapshot_file(path_oidn, variant = Sys.info()[["sysname"]])
   }
 })
+
+test_that("Test different rendering integrator types, snapshot", {
+  set.seed(1)
+  types = c("rtiow", "nee", "basic")
+  paths = tempfile(pattern=types, fileext = ".png")
+  for(i in seq_along(types)) {
+    set.seed(1)
+    generate_cornell() |> 
+      render_scene(samples=16, denoise = FALSE, preview = FALSE, 
+                   filename=paths[i], integrator_type = types[i])
+    expect_snapshot_file(paths[i], variant = Sys.info()[["sysname"]])
+  }
+  if(rayrender:::cppdef_HAS_OIDN()) {
+    types_oidn = c("rtiow", "nee", "basic")
+    paths_oidn = tempfile(pattern=paste0("denoise_",types_oidn), fileext = ".png")
+
+    for(i in seq_along(types)) {
+      set.seed(1)
+      generate_cornell() |> 
+        render_scene(samples=16, denoise = TRUE, preview = FALSE, 
+                     filename=paths_oidn[i], integrator_type = types_oidn[i])
+      expect_snapshot_file(paths_oidn[i], variant = Sys.info()[["sysname"]])
+    }
+  }
+})
