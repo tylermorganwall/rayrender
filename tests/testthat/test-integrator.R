6b149300b37af709506f74a2369525c49066499f
tylermorganwall <tylermorganwall@users.noreply.github.com>
2025-01-05 23:26:27 -0500

    -Update tests

--- tests/testthat/test-integrator.R
+++ tests/testthat/test-integrator.R
@@ -4,19 +4,19 @@ test_that("Test different rendering integrator types, snapshot", {
   for(i in seq_along(types)) {
     set.seed(i)
     expect_snapshot_output({generate_cornell() |> 
-      render_scene(samples=4, denoise = FALSE, preview = FALSE,  width=6, height=6,
-                  filename = tempfile(fileext = ".png"), 
+      render_scene(samples=1, denoise = FALSE, preview = FALSE,  width=6, height=6,
+                  filename = tempfile(fileext = ".png"), parallel = FALSE,
                   integrator_type = types[i])}, variant = Sys.info()[["sysname"]])
   }
-  if(rayrender:::cppdef_HAS_OIDN()) {
-    types_oidn = c("rtiow", "nee", "basic")
+  # if(rayrender:::cppdef_HAS_OIDN()) {
+  #   types_oidn = c("rtiow", "nee", "basic")
 
-    for(i in seq_along(types)) {
-      set.seed(i+3)
-      expect_snapshot_output({generate_cornell() |> 
-        render_scene(samples=4, denoise = TRUE, preview = FALSE,  width=6, height=6,
-                     filename = tempfile(fileext = ".png"),
-                     integrator_type = types_oidn[i])}, variant = Sys.info()[["sysname"]])
-    }
-  }
+  #   for(i in seq_along(types)) {
+  #     set.seed(i+3)
+  #     expect_snapshot_output({generate_cornell() |> 
+  #       render_scene(samples=1, denoise = TRUE, preview = FALSE,  width=6, height=6,
+  #                    filename = tempfile(fileext = ".png"), parallel = FALSE,
+  #                    integrator_type = types_oidn[i])}, variant = Sys.info()[["sysname"]])
+  #   }
+  # }
 })
