3453fb7b47cec5a07d226c58d79a20c56e3c1256
tylermorganwall <tylermorganwall@users.noreply.github.com>
2025-01-05 19:27:20 -0500

    -Update snapshot test for integrator

--- tests/testthat/test-integrator.R
+++ tests/testthat/test-integrator.R
@@ -2,18 +2,20 @@ test_that("Test different rendering integrator types, snapshot", {
   set.seed(1)
   types = c("rtiow", "nee", "basic")
   for(i in seq_along(types)) {
-    set.seed(1)
+    set.seed(i)
     expect_snapshot({generate_cornell() |> 
       render_scene(samples=4, denoise = FALSE, preview = FALSE,  width=6, height=6,
+                  filename = tempfile(fileext = ".png"), 
                   integrator_type = types[i])}, variant = Sys.info()[["sysname"]])
   }
   if(rayrender:::cppdef_HAS_OIDN()) {
     types_oidn = c("rtiow", "nee", "basic")
 
     for(i in seq_along(types)) {
-      set.seed(1)
+      set.seed(i+3)
        expect_snapshot({generate_cornell() |> 
         render_scene(samples=4, denoise = TRUE, preview = FALSE,  width=6, height=6,
+                     filename = tempfile(fileext = ".png"),
                      integrator_type = types_oidn[i])}, variant = Sys.info()[["sysname"]])
     }
   }
