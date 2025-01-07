d51a3e112bd5b7add99eaef8eee9c008774825a2
tylermorganwall <tylermorganwall@users.noreply.github.com>
2025-01-06 23:36:40 -0500

    -Add withr to Suggests and set rgl.useNULL option in rayshader

--- tests/testthat/test-rayshader.R
+++ tests/testthat/test-rayshader.R
@@ -6,17 +6,20 @@ compare_image = function(path1, path2) {
 
 
 test_that("rayshader tests", {
-  testthat::expect_no_error({
-    volcano %>%
-      rayshader::sphere_shade() %>%
-      rayshader::plot_3d(volcano,zscale = 2)
-    temp_hq = tempfile(fileext = ".png")
-    set.seed(1)
-    rayshader::render_highquality(filename=temp_hq, samples = 100)
-    rgl::close3d()
+  withr::with_options(list(rgl.useNULL = TRUE),
+    {
+    testthat::expect_no_error({
+      volcano %>%
+        rayshader::sphere_shade() %>%
+        rayshader::plot_3d(volcano,zscale = 2)
+      temp_hq = tempfile(fileext = ".png")
+      set.seed(1)
+      rayshader::render_highquality(filename=temp_hq, samples = 100)
+      rgl::close3d()
+    })
+    if(!isTRUE(as.logical(Sys.getenv("IS_GHA", "false")))) {
+      announce_snapshot_file(temp_hq, name = "rayshader.png")
+      expect_snapshot_file(temp_hq, name = "rayshader.png", compare = compare_image)
+    }
   })
-  if(!isTRUE(as.logical(Sys.getenv("IS_GHA", "false")))) {
-    announce_snapshot_file(temp_hq, name = "rayshader.png")
-    expect_snapshot_file(temp_hq, name = "rayshader.png", compare = compare_image)
-  }
 })
