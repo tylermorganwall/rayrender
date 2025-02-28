1ec310db15f522fd2e4685f52e207f45e617558f
tylermorganwall <tylermorganwall@users.noreply.github.com>
2025-02-27 22:01:04 -0500

    -Only run cpp tests when NOT_CRAN

--- tests/testthat/test-cpp.R
+++ tests/testthat/test-cpp.R
@@ -1,3 +1,3 @@
-if(length(find.package("xml2",quiet=TRUE)) > 0) {
+if(length(find.package("xml2",quiet=TRUE)) > 0 && isTRUE(as.logical(Sys.getenv("NOT_CRAN", "true")))) {
   run_cpp_tests("rayrender")
 }
